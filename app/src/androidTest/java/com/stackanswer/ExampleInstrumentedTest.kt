aa7852cb0fa259c16e7e71f1013de51380708b7f
kreasikode1 <kreasikode@gmail.com>
2021-03-28 15:34:05 +0700

    use rx java

--- app/src/androidTest/java/com/stackanswer/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/stackanswer/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package com.stackanswer
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
