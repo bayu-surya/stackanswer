fd511d4179e58943bb30506f7c36c8fc0ad11a44
kreasikode1 <kreasikode@gmail.com>
2021-04-02 01:08:36 +0700

    add Certificate Pinning

--- app/src/main/java/com/stackanswer/injection/CoreModule.kt
+++ app/src/main/java/com/stackanswer/injection/CoreModule.kt
@@ -17,6 +17,7 @@ import com.stackanswer.core.source.repository.*
 import com.stackanswer.core.utils.AppExecutors
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -28,10 +29,15 @@ import java.util.concurrent.TimeUnit
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
